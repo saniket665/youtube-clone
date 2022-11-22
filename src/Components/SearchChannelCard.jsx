bb4d4b979d4844f06e5352ecd749a1f91c323a0a
Aniket Singh <saniket665@gmail.com>
2022-11-22 22:16:06 +0530

    changed main icon

---
+++ src/Components/SearchChannelCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Box} from "@mui/material"
+
+const SearchChannelCard = ({video}) => {
+  return (
+    <Box>
+        <div>SearchChannelCard</div>
+    </Box> )
+}
+
+export default SearchChannelCard
